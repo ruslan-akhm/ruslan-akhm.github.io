d6bb6eaf86898999a9cec4d01d70f985381bc5bc
Ruslan Akhmetshin <ruslan.akhmetshin94@gmail.com>
2020-08-10 23:37:49 -0300

    Updates

---
+++ precache-manifest.26df74a12006879dec066fcba8ea9802.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "383cb3135b0642af3c2c899e13161929",
+    "url": "/ruslan-akhm.github.io/index.html"
+  },
+  {
+    "revision": "9052f10d13f032e46968",
+    "url": "/ruslan-akhm.github.io/static/css/main.b47deeec.chunk.css"
+  },
+  {
+    "revision": "18ce885e2328eda220de",
+    "url": "/ruslan-akhm.github.io/static/js/2.1f9a4381.chunk.js"
+  },
+  {
+    "revision": "229313a91dfc1a4229b4758368bd8914",
+    "url": "/ruslan-akhm.github.io/static/js/2.1f9a4381.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9052f10d13f032e46968",
+    "url": "/ruslan-akhm.github.io/static/js/main.640630b3.chunk.js"
+  },
+  {
+    "revision": "ebfba2909bf27fa5c909",
+    "url": "/ruslan-akhm.github.io/static/js/runtime-main.980aeaec.js"
+  },
+  {
+    "revision": "3bf2094bbdf51b6a35568759b53b7552",
+    "url": "/ruslan-akhm.github.io/static/media/1-min.3bf2094b.jpg"
+  },
+  {
+    "revision": "1dd43fa54494f685f8da8562c909c720",
+    "url": "/ruslan-akhm.github.io/static/media/2-min.1dd43fa5.jpg"
+  },
+  {
+    "revision": "b748c3f9c7b37db78630e13938a25b33",
+    "url": "/ruslan-akhm.github.io/static/media/3-min.b748c3f9.jpg"
+  }
+]);
