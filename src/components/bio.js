1304573a1e719da86c7fbf6c706befffe6ba6642
Ruslan Akhmetshin <ruslan.akhmetshin94@gmail.com>
2021-03-31 22:51:26 -0400

    upd bio

--- src/components/bio.js
+++ src/components/bio.js
@@ -35,12 +35,25 @@ const Bio = () => {
           <img src={me} alt="photo of me" />
         </div>
         <p>
-          Hi! My name is Ruslan, I am a Toronto-based developer and I like
-          crafting web applications by writing clean, readable and maintainable
-          code. I continuously expand my skills and knowledge and evolve as a
-          developer.
-          <br /> I also possess a background in Electronics Engineering and a
-          bachelor's degree in Automation.
+          Hi! My name is Ruslan. I am passionate about crafting things with
+          code. I've been around coding since University. My first experince of
+          programming was with PLCs where I studied several languages, mostly
+          graphical. First UI that I built was a HMI - human machine interface
+          for operators, working at the oilfileds, so they could see sensors'
+          readings and control valves.
+          <br />
+          <br />
+          During my college days I studied the very basics of computer science
+          as well as Python and C++ languages. I can surely say I have knowledge
+          of not just coding itself, but also 1's and 0's that stand behind it
+          as well as the physical layer of electronic signals.
+          <br />
+          <br />
+          For the past year and a half I've been into web software development.
+          I am interested in several technologies for web and mobile
+          development. I also possess interest in Machine Learning. I am looking
+          to grow further and would like to apply my skills in an actual
+          full-time role, where I could help the company to achieve it's goals.
         </p>
       </div>
       <div id="tech-box">{technologies}</div>
